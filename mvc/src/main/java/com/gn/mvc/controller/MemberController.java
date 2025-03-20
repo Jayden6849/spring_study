77e755eaf55749f18568b1489a71545b79fcc9f4
Jayden <jayden6849@gmail.com>
2025-03-20 15:48:07 +0900

    [Jayden] 회원 탈퇴 기능 - 세션 만료, 쿠키 제거, DB에서 제거

--- mvc/src/main/java/com/gn/mvc/controller/MemberController.java
+++ mvc/src/main/java/com/gn/mvc/controller/MemberController.java
@@ -5,14 +5,19 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.gn.mvc.dto.MemberDto;
+import com.gn.mvc.entity.Member;
 import com.gn.mvc.service.MemberService;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -41,11 +46,7 @@ public class MemberController {
 		resultMap.put("res_code", "500");
 		resultMap.put("result_msg", "회원가입 중 오류가 발생하였습니다.");
 		
-		System.out.println("크리에이트 전 : "+memberDto);
-		
 		MemberDto resultDto = service.createMember(memberDto);
-		
-		System.out.println("크리에이트 후 : "+memberDto);
 		
 		if(resultDto != null) {
 			resultMap.put("res_code", "200");
@@ -60,4 +61,61 @@ public class MemberController {
 		return "member/create";
 	}
 	
+	@GetMapping("/member/{id}/update")
+	public String updateMemberView(@PathVariable("id") Long memberNo, Model model) {
+		Member member = service.selectMemberOne(memberNo);
+		model.addAttribute("member", member);
+		return "member/update";
+	}
+	
+	@PostMapping("/member/{id}/update")
+	@ResponseBody
+	public Map<String, String> updateMemberApi(MemberDto memberDto, HttpServletResponse response) {
+		
+		Map<String,String> resultMap = new HashMap<>();
+		
+		resultMap.put("res_code", "500");
+		resultMap.put("res_msg", "회원 정보 수정 중 오류가 발생하였습니다.");
+		
+		int result = service.updateMember(memberDto);
+		
+		if(result > 0) {
+			// Cookie를 다루는 것은 비지니스 로직이지만, HttpServletResponse를 필요로 하기 때문에 Controller에서 다루는 것이 더 적절함
+			// 쿠키(remember-me) 무효화
+			Cookie rememberMe = new Cookie("remember-me", null);		// "remember-me"에 담긴 값을 null로 바꿈
+			rememberMe.setMaxAge(0);									// 해당 쿠기의 길이를 0
+			rememberMe.setPath("/");									// 모든 경로의 쿠기를 없애줌
+			response.addCookie(rememberMe);
+			
+			resultMap.put("res_code", "200");
+			resultMap.put("res_msg", "회원 정보 수정이 완료되었습니다.");
+		}
+		
+		return resultMap;
+		
+	}
+	
+	@DeleteMapping("/member/{id}")
+	@ResponseBody
+	public Map<String, String> deleteMemberAApi(@PathVariable("id") Long memberNo, HttpServletResponse response) {
+		
+		Map<String,String> resultMap = new HashMap<>();
+		
+		resultMap.put("res_code", "500");
+		resultMap.put("res_msg", "회원 탈퇴 중 오류가 발생하였습니다.");
+		
+		int result = service.deleteMember(memberNo);
+		
+		if(result > 0) {		
+			Cookie rememberMe = new Cookie("remember-me", null);
+			rememberMe.setMaxAge(0);
+			rememberMe.setPath("/");
+			response.addCookie(rememberMe);
+			
+			resultMap.put("res_code", "200");
+			resultMap.put("res_msg", "회원 탈퇴가 완료되었습니다.");
+		}
+		
+		return resultMap;
+	}
 }
--- mvc/src/main/java/com/gn/mvc/service/MemberService.java
+++ mvc/src/main/java/com/gn/mvc/service/MemberService.java
@@ -1,5 +1,13 @@
 package com.gn.mvc.service;
 
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +23,8 @@ public class MemberService {
 	
 	private final MemberRepository repository;
 	private final PasswordEncoder passwordEncoder;		// 회원가입할 때 암호화해주는 메소드
+	private final DataSource dataSource;
+	private final UserDetailsService userDetailsService;
 	
 	public MemberDto createMember(MemberDto memberDto) {
 		memberDto.setMember_pw(passwordEncoder.encode(memberDto.getMember_pw()));
@@ -22,6 +32,68 @@ public class MemberService {
 		Member result = repository.save(memberDto.toEntity());
 		
 		return new MemberDto().toDto(result);
+	}
+	
+	public Member selectMemberOne(Long memberNo) {
+		Member result = repository.findById(memberNo).orElse(null);
+		
+		return result;
+	}
+	
+	public int updateMember(MemberDto memberDto) {
+		
+		int result = 0;
+		
+		try {
+			// 1. 데이터베이스 회원 정보 수정
+			memberDto.setMember_pw(passwordEncoder.encode(memberDto.getMember_pw()));
+			
+			Member updated = repository.save(memberDto.toEntity());
+			
+			if(updated != null) {	
+				// 2. remember-me(** DB **, Cookie)가 있다면 무효화 - DB 는 서비스에서, Cookie는 컨트롤러에서 다루는 것이 적절함 
+				JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+				String sql = "DELETE FROM `persistent_logins` WHERE `username` = ?";
+				jdbcTemplate.update(sql, memberDto.getMember_id());
+				
+				// 3. 변경된 회원 정보 Security Context 에 즉시 반영
+				UserDetails updatedUserDetails = userDetailsService.loadUserByUsername(memberDto.getMember_id());
+				Authentication newAuth = new UsernamePasswordAuthenticationToken(updatedUserDetails, updatedUserDetails.getPassword(), updatedUserDetails.getAuthorities());
+				SecurityContextHolder.getContext().setAuthentication(newAuth);
+				
+				result = 1;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	public int deleteMember(Long memberNo) {
+		int result = 0;
+		
+		try {
+			Member target = repository.findById(memberNo).orElse(null);
+			MemberDto memberDto = new MemberDto().toDto(target);
+			
+			if(target != null) {
+				
+				repository.delete(target);
+				
+				JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+				String sql = "DELETE FROM `persistent_logins` WHERE `username` = ?";
+				jdbcTemplate.update(sql, memberDto.getMember_id());
+				
+				SecurityContextHolder.getContext().setAuthentication(null);
+
+				result = 1;
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 	
 }
